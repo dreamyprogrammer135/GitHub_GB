bc63950f3de10605a251c829267249d2f07ed266
dreamyprogrammer135 <dreamyprogrammer135@gmail.com>
2023-03-13 14:58:47 +0300

    Init commit

---
+++ app/src/main/java/ru/super135/github_gb/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.super135.github_gb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
